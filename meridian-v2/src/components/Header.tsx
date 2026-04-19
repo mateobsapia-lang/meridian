700dfa8414b66ce35a85e46a3f0312b7929f7571
mateobsapia-lang <mateobsapia@gmail.com>
2026-04-19 18:48:54 -0300

    Update Header.tsx

--- meridian-v2/src/components/Header.tsx
+++ meridian-v2/src/components/Header.tsx
@@ -40,7 +40,7 @@ export function Header() {
                 </button>
               </div>
             ) : (
-             <div className="flex gap-2 items-center">
+              <div className="flex gap-2 items-center">
                 <button onClick={() => setLoginModalOpen(true)} className="inline-flex btn-ghost !py-[9px] !px-3 sm:!px-4 !text-[9px]">Ingresar</button>
                 <button onClick={() => setLoginModalOpen(true)} className="btn-primary !py-[9px] !px-3 sm:!px-4 !text-[9px] whitespace-nowrap">Listar</button>
               </div>
